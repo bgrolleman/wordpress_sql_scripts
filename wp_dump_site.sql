143b981f4f95c29c98e72ecbfddab70c2de93840
Bas Grolleman <bgrolleman@emendo-it.nl>
2012-07-23 21:11:24 +0200

    Small bugfix

--- wp_dump_site.sql
+++ wp_dump_site.sql
@@ -2,4 +2,4 @@
 #
 # Create a SQL dump of a single site in a wordpress multi user install
 . settings
-echo "show tables like '${PREFIX}\_$1\_%'" | mysql --no-column-names  $DB | xargs mysqldump $DB 
+echo "show tables like '${PREFIX}\_$1\_%'" | mysql -N $DB | xargs mysqldump $DB 
